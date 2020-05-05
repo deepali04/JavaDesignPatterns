a873837313a992ca60103b54abc12e82f35628b3
deepali04 <nagwade.deepali@gmail.com>
2020-05-06 02:54:26 +0530

    Added Behavioural Patterns

--- Behavioural Design Patterns/ObserverDesignPattern/src/com/behavioural/observer/Observer.java
+++
@@ -1,6 +0,0 @@
-package com.behavioural.observer;
-
-public interface Observer {
-
-	public void update(Status s);
-}
--- Behavioural Design Patterns/ObserverDesignPattern/src/com/behavioural/observer/User.java
+++
@@ -1,8 +0,0 @@
-package com.behavioural.observer;
-
-public interface User {
-
-	public void add(Observer o);
-	public void delete(Observer o);
-	public void notifyUpdate(Status s);
-}

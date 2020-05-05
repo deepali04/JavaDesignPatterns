60c22556a8f198493c1554e3dca72ccb9064da46
deepali04 <nagwade.deepali@gmail.com>
2020-05-06 02:56:58 +0530

    Added Observer Pattern

---
+++ Behavioural Design Patterns/ObserverDesignPattern/src/com/behavioural/observer/Status.java
@@ -0,0 +1,15 @@
+package com.behavioural.observer;
+
+public class Status {
+	
+	final String statusContent;
+	
+	public Status(String m) {
+		this.statusContent=m;
+	}
+	
+	public String getStatusContent() {
+		return statusContent;
+	}
+
+}
